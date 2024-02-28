a035085252592f1b784b43d2e18041ef0d37581c
NickGroenl <bartocanitrot1@hotmail.com>
2024-02-28 05:41:59 -0300

    Signup view

--- components/dashboard/Footer/index.tsx
+++ components/dashboard/Footer/index.tsx
@@ -15,24 +15,22 @@ const Footer = () => {
               <div className="mb-12 max-w-[360px] lg:mb-16">
                 <Link href="/" className="mb-8 inline-block">
                   <Image
-                    src="/images/logo/logo-2.svg"
+                    src="/images/logo/logopreferente.png"
                     alt="logo"
                     className="w-full dark:hidden"
                     width={140}
                     height={30}
+                    style={{transform: 'translateX(-15%)'}}
                   />
                   <Image
-                    src="/images/logo/logo.svg"
+                    src="/images/logo/logopreferente.png"
                     alt="logo"
                     className="hidden w-full dark:block"
                     width={140}
                     height={30}
                   />
                 </Link>
-                <p className="dark:text-body-color-dark mb-9 text-base leading-relaxed text-body-color">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer lobortis.
-                </p>
+                
                 <div className="flex items-center">
                   <a
                     href="/"
