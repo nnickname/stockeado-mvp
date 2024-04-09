316a6c0eb0fff33c4a69d0a8bdf1f32c89b1bb0a
NickGroenl <bartocanitrot1@hotmail.com>
2024-04-09 08:47:54 -0300

    try

--- next.config.js
+++ next.config.js
@@ -13,23 +13,15 @@ const nextConfig = {
   },
   env: {
     API_KEY: process.env.NEXT_PUBLIC_API_TOKEN,
-  }
-};
-
-
-module.exports = nextConfig, {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
+  },
+  source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
-      }
-    ]
-  }
 };
+
+module.exports = nextConfig;
+
