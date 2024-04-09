d7f3443c057cccfd028480e55e3e7fda10286ede
NickGroenl <bartocanitrot1@hotmail.com>
2024-04-09 08:26:02 -0300

    try

--- app/api/db.ts
+++ app/api/db.ts
@@ -14,14 +14,19 @@ const corsOptions ={
   optionSuccessStatus:200,
   
 }
+function setCorsHeaders(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+}
 function createServer() {
   const app = express();
   app.use(express.json());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(cookieParser());
- 
-  app.use(cors(corsOptions))
+  app.use(setCorsHeaders);
   return app;
 }
 
--- dist/server/app/api/admin/route.js
+++ dist/server/app/api/admin/route.js
@@ -263,7 +263,7 @@ function middlewareApi() {
 var __webpack_require__ = require("../../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [8478,1835,3680,6971,3928], () => (__webpack_exec__(45227)));
+var __webpack_exports__ = __webpack_require__.X(0, [8478,1835,5177,6971,3928], () => (__webpack_exec__(45227)));
 module.exports = __webpack_exports__;
 
 })();
